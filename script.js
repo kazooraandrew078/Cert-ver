91bed432f206a5e6875d7e2eb22826dd82901131
kazooraandrew078 <kazooraandrew078@gmail.com>
2026-09-24 04:58:31 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
-const certificates = { "CERT001": { name: "RO/15053 CAPT Moses Atweta", courseAttended: "Digital Tactical Operations Centre (DTOC)", intake: "01/26", dateIssued: "Sep 26", institution: "UPDF- IPSO-TC Singo" },"CERT002": {    name: "RO/13523 LT Joseph Mwesige",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT003": {    name: "RO/17011 LT Dennis Niwagaba",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT004": {    name: "RO/18220 LT Allan Egwele",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT005": {    name: "RO/18300 LT Ochwo Omukaga",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT006": {    name: "RA/191435 S/SGT Tumwetabe Saison",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT007": {    name: "RA/208992 SGT Muheirwe Everesto",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT008": {    name: "RA/229363 CPL Ebulu Micheal",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT009": {    name: "RA/210073 L/CPL Atamba Amos",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT010": {    name: "RA/230091 L/CPL Mayebe Simon",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT011": {    name: "RA/232820 L/CPL Kagudemungalo Mesach",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT012": {    name: "RA/239769 L/CPL Kato Abdulhakim",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT013": {    name: "RA/254204 L/CPL Jemba Chrispus Pius",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT014": {    name: "RA/258614 L/CPL Loum Benard Achola",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT015": {    name: "RA/260542 L/CPL Okopor Micheal",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT016": {    name: "RA/261851 L/CPL Otim Emmanuel",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT017": {    name: "RA/265050 L/CPL Ewanyu Joel Sadric Baiwa",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT018": {    name: "RA/281648 L/CPL Tusuubira Samson",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT019": {    name: "RA/285314 L/CPL Golya Kefa",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT020": {    name: "RA/300780 L/CPL Acwici Emmily",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT021": {    name: "RA/300913 L/CPL Amunyos Paskal",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT022": {    name: "RA/301397 L/CPL Kwemoi Jabeth",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT023": {    name: "RA/301418 L/CPL Lodonga Micheal",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT024": {    name: "RA/301725 L/CPL Njunoha Fred",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT025": {    name: "RA/301737 L/CPL Nuwamanya Aaron",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT026": {    name: "RA/243850 PTE Chelangat Isaac",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT027": {    name: "RA/251569 PTE Lumansi Kevin",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT028": {    name: "RA/253483 PTE Olanya Richard",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT029": {    name: "RA/253812 PTE Aupal Cryspus Emmanuel",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT030": {    name: "RA/255937 PTE Ogole Daniel",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT031": {    name: "RA/259775 PTE Malinga Denis",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT032": {    name: "RA/263383 PTE Kisakye Aisha Namboga",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT033": {    name: "RA/267357 PTE Etamu Morris",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT034": {    name: "RA/269144 PTE Turyamwesimira K Redstuart",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT035": {    name: "RA/273402 PTE Businge Darlson",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT036": {    name: "RA/274666 PTE Aluma Gilbert",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT037": {    name: "RA/274720 PTE Amecho Jesca",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT038": {    name: "RA/275675 PTE Eliba Denis",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT039": {    name: "RA/275908 PTE Sunday Godwil",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT040": {    name: "RA/278922 PTE Otubo Derrick",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT041": {    name: "RA/280387 PTE Kamurungi Rose",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT042": {    name: "RA/281948 PTE Atwiziire Clare",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT043": {    name: "RA/284864 PTE Chelangat Linda",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT044": {    name: "RA/286486 PTE Natuhweza Mohammed",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT045": {    name: "RA/289933 PTE Kazoora Andrew",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT046": {    name: "RA/292598 PTE Agaba Juspus Guma",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"},
-"CERT047": {    name: "RA/295873 PTE Omoding Vicent",    courseAttended: "Digital Tactical Operations Centre (DTOC)",    intake: "01/26",    dateIssued: "Sep 26",    institution: "UPDF- IPSO-TC Singo"}};function verifyCertificate() { const certificateNumber = document .getElementById("certificateNumber") .value .trim() .toUpperCase();const result = document.getElementById("result");
-if (certificates[certificateNumber]) {    const certificate = certificates[certificateNumber];
-    result.innerHTML = `        <h3>Certificate Verified</h3>        <p><strong>Certificate Number:</strong> ${certificateNumber}</p>        <p><strong>Name:</strong> ${certificate.name}</p>        <p><strong>Course Attended:</strong> ${certificate.courseAttended}</p>        <p><strong>Intake:</strong> ${certificate.intake}</p>        <p><strong>Date Issued:</strong> ${certificate.dateIssued}</p>        <p><strong>Institution:</strong> ${certificate.institution}</p>    `;} <p><strong>Date Issued:</strong> $
-{certificate.dateIssued}</p> <p><strong>Institution:</strong> $
-{certificate. institution}</p>else {    result.innerHTML = `        <h3>Certificate Not Found</h3>        <p>Please check the certificate number and try again.</p>    `;}}
+const certificates = {    "CERT001": {        serviceNumber: "RO/15053",        rank: "Capt",        name: "Moses Atweta"    },
+    "CERT002": {        serviceNumber: "RO/13523",        rank: "Lt",        name: "Joseph Mwesige"    },
+    "CERT003": {        serviceNumber: "RO/17011",        rank: "Lt",        name: "Dennis Niwagaba"    },
+    "CERT004": {        serviceNumber: "RO/18220",        rank: "Lt",        name: "Allan Egwele"    },
+    "CERT005": {        serviceNumber: "RO/18300",        rank: "Lt",        name: "Ochwo Omukaga"    },
+    "CERT006": {        serviceNumber: "RA/191435",        rank: "S/Sgt",        name: "Tumwetabe Saison"    },
+    "CERT007": {        serviceNumber: "RA/208992",        rank: "Sgt",        name: "Muheirwe Everesto"    },
+    "CERT008": {        serviceNumber: "RA/229363",        rank: "Cpl",        name: "Ebulu Micheal"    },
+    "CERT009": {        serviceNumber: "RA/210073",        rank: "L/Cpl",        name: "Atamba Amos"    },
+    "CERT010": {        serviceNumber: "RA/230091",        rank: "L/Cpl",        name: "Mayebe Simon"    },
+    "CERT011": {        serviceNumber: "RA/232820",        rank: "L/Cpl",        name: "Kagudemungalo Mesach"    },
+    "CERT012": {        serviceNumber: "RA/239769",        rank: "L/Cpl",        name: "Kato Abdulhakim"    },
+    "CERT013": {        serviceNumber: "RA/254204",        rank: "L/Cpl",        name: "Jemba Chrispus Pius"    },
+    "CERT014": {        serviceNumber: "RA/258614",        rank: "L/Cpl",        name: "Loum Benard Achola"    },
+    "CERT015": {        serviceNumber: "RA/260542",        rank: "L/Cpl",        name: "Okopor Micheal"    },
+    "CERT016": {        serviceNumber: "RA/261851",        rank: "L/Cpl",        name: "Otim Emmanuel"    },
+    "CERT017": {        serviceNumber: "RA/265050",        rank: "L/Cpl",        name: "Ewanyu Joel Sadric Baiwa"    },
+    "CERT018": {        serviceNumber: "RA/281648",        rank: "L/Cpl",        name: "Tusuubira Samson"    },
+    "CERT019": {        serviceNumber: "RA/285314",        rank: "L/Cpl",        name: "Golya Kefa"    },
+    "CERT020": {        serviceNumber: "RA/300780",        rank: "L/Cpl",        name: "Acwici Emmily"    },
+    "CERT021": {        serviceNumber: "RA/300913",        rank: "L/Cpl",        name: "Amunyos Paskal"    },
+    "CERT022": {        serviceNumber: "RA/301397",        rank: "L/Cpl",        name: "Kwemoi Jabeth"    },
+    "CERT023": {        serviceNumber: "RA/301418",        rank: "L/Cpl",        name: "Lodonga Micheal"    },
+    "CERT024": {        serviceNumber: "RA/301725",        rank: "L/Cpl",        name: "Njunoha Fred"    },
+    "CERT025": {        serviceNumber: "RA/301737",        rank: "L/Cpl",        name: "Nuwamanya Aaron"    },
+    "CERT026": {        serviceNumber: "RA/243850",        rank: "Pte",        name: "Chelangat Isaac"    },
+    "CERT027": {        serviceNumber: "RA/251569",        rank: "Pte",        name: "Lumansi Kevin"    },
+    "CERT028": {        serviceNumber: "RA/253483",        rank: "Pte",        name: "Olanya Richard"    },
+    "CERT029": {        serviceNumber: "RA/253812",        rank: "Pte",        name: "Aupal Cryspus Emmanuel"    },
+    "CERT030": {        serviceNumber: "RA/255937",        rank: "Pte",        name: "Ogole Daniel"    },
+    "CERT031": {        serviceNumber: "RA/259775",        rank: "Pte",        name: "Malinga Denis"    },
+    "CERT032": {        serviceNumber: "RA/263383",        rank: "Pte",        name: "Kisakye Aisha Namboga"    },
+    "CERT033": {        serviceNumber: "RA/267357",        rank: "Pte",        name: "Etamu Morris"    },
+    "CERT034": {        serviceNumber: "RA/269144",        rank: "Pte",        name: "Turyamwesimira K Redstuart"    },
+    "CERT035": {        serviceNumber: "RA/273402",        rank: "Pte",        name: "Businge Darlson"    },
+    "CERT036": {        serviceNumber: "RA/274666",        rank: "Pte",        name: "Aluma Gilbert"    },
+    "CERT037": {        serviceNumber: "RA/274720",        rank: "Pte",        name: "Amecho Jesca"    },
+    "CERT038": {        serviceNumber: "RA/275675",        rank: "Pte",        name: "Eliba Denis"    },
+    "CERT039": {        serviceNumber: "RA/275908",        rank: "Pte",        name: "Sunday Godwil"    },
+    "CERT040": {        serviceNumber: "RA/278922",        rank: "Pte",        name: "Otubo Derrick"    },
+    "CERT041": {        serviceNumber: "RA/280387",        rank: "Pte",        name: "Kamurungi Rose"    },
+    "CERT042": {        serviceNumber: "RA/281948",        rank: "Pte",        name: "Atwiziire Clare"    },
+    "CERT043": {        serviceNumber: "RA/284864",        rank: "Pte",        name: "Chelangat Linda"    },
+    "CERT044": {        serviceNumber: "RA/286486",        rank: "Pte",        name: "Natuhweza Mohammed"    },
+    "CERT045": {        serviceNumber: "RA/289933",        rank: "Pte",        name: "Kazoora Andrew"    },
+    "CERT046": {        serviceNumber: "RA/292598",        rank: "Pte",        name: "Agaba Juspus Guma"    },
+    "CERT047": {        serviceNumber: "RA/295873",        rank: "Pte",        name: "Omoding Vicent"    }};
+
+function verifyCertificate() {
+    const certificateNumber = document        .getElementById("certificateNumber")        .value        .trim()        .toUpperCase();
+    const result = document.getElementById("result");
+    if (certificates[certificateNumber]) {
+        const certificate = certificates[certificateNumber];
+        result.innerHTML = `            <h3>Certificate Verified</h3>
+            <p><strong>CERT. NUMBER:</strong> ${certificateNumber}</p>
+            <p><strong>SERVICE NUMBER:</strong> ${certificate.serviceNumber}</p>
+            <p><strong>NAME:</strong> ${certificate.name}</p>
+            <p><strong>RANK:</strong> ${certificate.rank}</p>
+            <p><strong>COURSE ATTENDED:</strong>            Digital Tactical Operations Centre (DTOC)</p>
+            <p><strong>INTAKE:</strong> 01/26</p>
+            <p><strong>INSTITUTION:</strong> UPDF- IPSO-TC Singo</p>
+            <p><strong>DATE ISSUED:</strong> Sep 26</p>
+            <br>
+            <p><strong>ISSUING OFFICER:</strong></p>
+            <p>[SIGNATURE]</p>
+            <p><strong>NAME:</strong> MB KIMUKU</p>
+            <p><strong>RANK:</strong> Lt. Col</p>
+            <p><strong>APPOINTMENT:</strong> Chief Instructor</p>        `;
+    } else {
+        result.innerHTML = `            <p style="color:red;">                Certificate not found. Please check the certificate number.            </p>        `;    }}
